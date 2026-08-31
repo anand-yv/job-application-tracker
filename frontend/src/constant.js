dda8e842ccf6f44f0bfe9e0be7818541e4984d41
anand-yv <anand93056ad@gmail.com>
2026-08-31 19:10:51 +0530

    feat : added the theme context provider

--- frontend/src/constant.js
+++ frontend/src/constant.js
@@ -1,1 +1,5 @@
 export const TOKEN_KEY = "access_token";
+
+export const THEME_KEY = "theme";
+export const LIGHT_THEME = "light";
+export const DARK_THEME = "dark";
---
+++ frontend/src/context/ThemeContext.jsx
@@ -0,0 +1,34 @@
+import { createContext, useEffect, useState } from "react";
+import { DARK_THEME, LIGHT_THEME, THEME_KEY } from "../constant";
+
+
+const ThemeContext = createContext();
+
+const getInitialTheme = () => {
+    const saved = localStorage.getItem(THEME_KEY);
+    if(saved) return saved;
+
+    const systemPrefersDark = window.matchMedia("(prefer-color-scheme: dark)").matches;
+    return systemPrefersDark ? DARK_THEME : LIGHT_THEME;
+}
+
+export const ThemeProvider = ({children}) => {
+    const [theme, setTheme] = useState(getInitialTheme);
+
+    const toogleTheme = () => {
+        setTheme((prev) => (prev == LIGHT_THEME ? DARK_THEME : LIGHT_THEME));
+    }
+
+    useEffect(() => {
+        document.documentElement.setAttribute("data-theme" , theme);
+        localStorage.setItem(THEME_KEY, theme);
+    }, [theme])
+
+    return (
+        <ThemeContext.Provider value={{theme, toogleTheme}}>
+            {children}
+        </ThemeContext.Provider>
+    )
+
+}
+export default ThemeContext;
